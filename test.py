f4f5d376f84a252103a7aca3d4b037c691dcfa19
Impana Rajesh <impanarajesh@Impanas-MacBook-Pro.local>
2026-07-06 21:41:52 -0400

    final changes

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res=tavily_search("Best Hotels in Europe for Solo Travelers")
 # print(res)
 
-res=search_flights("Plan a 7 days Japan trip from Bangladesh")
+# res=search_flights("Plan a 7 days Japan trip from Bangladesh")
+# print(res)
+
+res=run_travel_agent("Plan a 7 days Japan trip from Bangladesh")
 print(res)
